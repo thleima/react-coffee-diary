419325c6ee9daeb61f070d0cda1677e8aad01124
thleima <thomas61.amiel@gmail.com>
2025-03-30 15:23:50 +0200

    link in footer

--- src/component/layout/Footer.tsx
+++ src/component/layout/Footer.tsx
@@ -1,7 +1,13 @@
 export default function Footer() {
 	return (
 		<footer>
-			<p>© 2025 My Coffee Diary</p>
+			<p>
+				© 2025 Coffee Diary by{" "}
+				<a href="https://github.com/thleima/react-coffee-diary" target="_blank">
+					thleima
+					<i className="fa-brands fa-square-github"></i>
+				</a>
+			</p>
 		</footer>
 	);
 }
